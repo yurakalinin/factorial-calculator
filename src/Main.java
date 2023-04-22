8bd793c5daf1728fc129ce3593255a995a2b015b
Yuriy Kalinin <yura_str@bk.ru>
2023-04-22 20:53:15 +0500

    initial

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Введите число, факториал которого вудем вычислять:");
+        int value = new Scanner(System.in).nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= value; i++){
+            factorial = factorial * i;
+        }
+
+        System.out.println(value + "! = " + factorial);
+    }
+}
